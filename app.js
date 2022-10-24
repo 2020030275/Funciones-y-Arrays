1c2c2b43ab2dc3d9d5bde1e7e101f891a401e3ce
2020030275 <2020030275@upsin.edu.mx>
2022-10-24 01:48:57 -0600

    Pares

--- app.js
+++ app.js
@@ -14,4 +14,21 @@ function promedio(){
     prom.innerHTML = res
     
 }
+function pares(){
+
+    let pares = document.getElementById('pareslb')
+
+    let array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+    let npares = 0
+
+    for(let con=0; con < array.length; ++con){
+        if((array[con] %2) == 0){
+            npares += 1
+        }
+    }
+       
+    pares.innerHTML = npares 
+}
+
+
 
