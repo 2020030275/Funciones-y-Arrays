bba98c96e213991d50bc427c3307f962a770bbf9
2020030275 <2020030275@upsin.edu.mx>
2022-10-24 01:48:34 -0600

    Promedio

---
+++ app.js
@@ -0,0 +1,17 @@
+function promedio(){
+
+    let prom = document.getElementById('prom')
+
+    let array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+    let res = 0
+
+    for(let con = 0; con < array.length; ++con){
+        res = res + array[con]
+    }
+
+    res = res / array.length;
+
+    prom.innerHTML = res
+    
+}
+
